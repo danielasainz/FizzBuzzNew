fc2724ae0c330e33ccc9ebaa0c10a28088644e42
danielasainz <dasainz@email.wm.edu>
2018-03-30 12:58:28 -0400

    user now determines the ending number

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,24 +1,31 @@
 package com.company;
 
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Numbers from 1 to 100");
-        for (int i = 1; i <= 100; i++) {
-            if (i % 5 == 0 && i % 3 ==0) {
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Enter your number: ");
+        int x = scan.nextInt();
+        scan.nextLine();
+
+        System.out.println("Numbers from 1 to your number");
+
+        for (int i = 1; i <= x; i++) {
+            if (i % 5 == 0 && i % 3 == 0) {
                 System.out.println("fizzbuzz");
-            }
-            else if (i % 3 == 0) {
+            } else if (i % 3 == 0) {
                 System.out.println("fizz");
             } else if (i % 5 == 0) {
-                System.out.println("buzz");}
-                else{
+                System.out.println("buzz");
+            } else {
                 System.out.println(i);
-
             }
-
         }
-
     }
 }
+
